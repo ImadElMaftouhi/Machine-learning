d054f896de59a6e436dcc9537e33b2bf084c4cbf
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2026-05-06 15:30:13 +0100

    fix(regression): Enhanced the code structure
    1. Changed the variable self.weights to self.coef_
    2. Addede self.intercept_
    3. fixed self.loss_history crashing due to * operator, changed to dot product using @.
    4. corrected the assert syntax.

--- classical_ml/regression/linear_regression.py
+++ classical_ml/regression/linear_regression.py
@@ -4,11 +4,13 @@ np.random.seed(42)
 
 class LinearRegression:
     def __init__(self, solver="normal", lr=0.001, n_iter=1000):
+        assert solver in ["normal", "gradient_descent", "auto"], "solver should be in (normal, gradient_descent, auto)"
         self.solver = solver
         self.lr = lr
         self.n_iter = n_iter
-        self.weights = None
+        self.coef_ = None
         self.loss_history = []
+        self.intercept_ = None
 
     def __str__(self):
         return f"LinearRegression Algorithm"
@@ -19,19 +21,20 @@ class LinearRegression:
     def fit(self,X:np.ndarray,y:np.ndarray):
         X_b = self._add_bias(X)
         if self.solver == "normal":
-            self.weights = np.linalg.pinv(X_b.T @ X_b) @  X_b.T @ y
+            self.coef_ = np.linalg.pinv(X_b.T @ X_b) @  X_b.T @ y
         elif self.solver =="gradient_descent":
             n = X_b.shape[0]
-            self.weights = np.zeros(X_b.shape[1])
+            self.coef_ = np.zeros(X_b.shape[1])
             for _ in range(self.n_iter):
-                residuals = X_b @ self.weights - y
+                residuals = X_b @ self.coef_ - y
                 grad = (2/n) * X_b.T @ residuals       
-                self.weights -= self.lr * grad
-                self.loss_history.append(float(residuals*residuals))
+                self.coef_ -= self.lr * grad
+                self.loss_history.append(float(residuals @ residuals))
+        if self.coef_ is not None: self.intercept_ = self.coef_[0]
         return self
         
     def predict(self, X):
         X_b = self._add_bias(X)
-        if self.weights == None:
+        if self.coef_ is None:
             raise ValueError("No weights computed. Try fitting the algorithm using LinearRegression.fit()")
-        return X_b @ self.weights  
+        return X_b @ self.coef_
