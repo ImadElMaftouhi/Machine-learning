a90e1e45694b9b3b323bfe3f5a33c0ac81428707
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2026-05-13 23:23:16 +0100

    feat(classification): Initialise LogisticRegression Class

---
+++ classical_ml/classification/logistic_regression.py
@@ -0,0 +1,20 @@
+# classification/logistic_regression.py
+import numpy as np
+
+class LogisticRegression:
+    def __init(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def __str__(self):
+        return "Logistic Regression Classifier"
+    
+    def __repr__(self) -> str:
+        return f"LogisticRegression()"
+    
+    
